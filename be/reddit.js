4753987180f606493838b24cce85a57c59618235
atusu <alisiatusu@gmail.com>
2023-01-08 22:07:10 +0200

    code refactoring + comments for easy readability

--- be/reddit.js
+++ be/reddit.js
@@ -1,12 +1,13 @@
 import fetch from "node-fetch";
 
 export async function callRedditAPI(user){
+
+    //Making a request for an access token
     const id = "oJTFZeJYktP5vz8F2lpb7g";
     const secret = "seDYWm2JafPR92cgN5RhCRw0KKa4eA";
     const basicAuth = Buffer.from(`${id}:${secret}`).toString("base64");
     const username = "Remarkable-Day324";
     const password = "9TmM.QA4/%gGhnS";
-
     const params = new URLSearchParams();
     params.append("grant_type", "password");
     params.append("username", username);
@@ -22,9 +23,9 @@ export async function callRedditAPI(user){
 
     const data = await response.json();
     let access_token = data.access_token;
-    //-------------------------------------------------
-    let newuser = user;
-    const responseForUser = await fetch(`https://oauth.reddit.com/user/${newuser}/submitted`, {
+
+    //now that we have the token, we can have access to what we need
+    const responseForUser = await fetch(`https://oauth.reddit.com/user/${user}/submitted`, {
         method: "GET",
         headers: {
             Authorization: `bearer ${access_token}`
@@ -32,47 +33,28 @@ export async function callRedditAPI(user){
     });
     
     const dataForUser = await responseForUser.json();
-    //-------------------------------------------------
-    let responseForServer = {};
-    let state = 0;
+    let responseForServer = {};    
     let posts = [];
-    //-------------------------------------------------
-    //checks if the user exists
-    try {
-        posts = dataForUser.data.children;
-        //checks if the user exists and has posts
-        if(posts.length > 0){
-            //each post's text is added to an array which is passed to the server response object
-            for(let i = 0; i < posts.length; i++){
-                posts[i] = posts[i].data.selftext;
-                //state = 1 -- if user exists and has posts
-                state = 1;
-            }
-        }else{
-            //state = 2 -- if the user exists but has no posts
-            state = 2;
-        }
-        
-    } catch (error) {
-        //state = 3 -- if the user doesn't exist
-        state = 3;
+
+    //checks if the user does not exist and returns the corresponding response
+    if(!dataForUser.hasOwnProperty("data")){
+        return responseForServer = {"posts":"0", "status":"204", "error":true, "error-message":"No user / empty input"}; 
     }
-    //-------------------------------------------------
-    switch (state) {
-        case 1:
-            responseForServer = {"posts": posts, "status":"200", "error": false, "error-message":"We gucci"};
-            break;
-        case 2:
-            responseForServer = {"posts":"0", "status":"204", "error":true, "error-message":"No posts for this user"};
-            break;
-        case 3:
-            responseForServer = {"posts":"0", "status":"204", "error":true, "error-message":"No user/empty input"};
-            break;
-        default:
-            break;
+    posts = dataForUser.data.children;
+
+    //checks if the user has no posts and returns the corresponding response
+    if(posts.length == 0){
+        return responseForServer = {"posts":"0", "status":"204", "error":true, "error-message":"No posts for this user"};
     }
 
-    return responseForServer;
+    //if it got here then it means that the user exists and also has posts
+    //will put all posts in an array and pass it to the response object
+    for(let i = 0; i < posts.length; i++){
+        posts[i] = posts[i].data.selftext;            
+    }
+
+    return responseForServer = {"posts": posts, "status":"200", "error": false, "error-message":"We gucci"};
+    
 }
 
 
--- be/twitter.js
+++ be/twitter.js
@@ -14,48 +14,27 @@ export async function callTwitterAPI(username){
     let state = 0;
     let data = []; 
     
-    //check if the user exists and has posts 
-    if(usernameData.hasOwnProperty("data")){
-        const userId = parseInt(usernameData["data"][0]["id"]);  
-        const tweetFields = "id,author_id,text,created_at" 
-        const url2 = `https://api.twitter.com/2/users/${userId}/tweets`;
-        const postsResponse = await fetch(url2, options)     
-        const postsData = await postsResponse.json()
-        
-        //check if the user has posts
-        if(postsData.hasOwnProperty("data")){
-            data = postsData.data;
-            //state = 1 -- if the user exists and has posts
-            state = 1;
-            //each post's text is added to an array which is passed to the server response object
-            for(let i = 0; i < data.length; i++) {
-                data[i] = data[i].text;
-            }
-            //-------------------------------------------------
-        }else{
-            //state = 2 -- if the user exists, but has no posts
-            state = 2;
-        }
-        //-------------------------------------------------
-    }else{
-        //state = 3 -- user doesn't exist or empty input
-        state = 3;
-    }  
-
-    switch (state) {
-        case 1:
-            responseForServer = {"posts":data, "error": false, "error-message":"We gucci", "status":"200"};   
-            break;
-        case 2:
-            responseForServer = {"posts":"0", "error":true, "error-message":"The user has no posts", "status":"204"}; 
-            break;
-        case 3: 
-            responseForServer = {"posts":"0", "error":true, "error-message":"No user / empty input", "status":"204"}; 
-        default:
-            break;
+    //checks if the user exists and returns the corresponding response object in case not
+    if(!usernameData.hasOwnProperty("data")){
+        return responseForServer = {"posts":"0", "error":true, "error-message":"No user / empty input", "status":"204"};
     }
-
-    return responseForServer;
-
+    //we know that the user exits so now we are extracting their posts
+    const userId = parseInt(usernameData["data"][0]["id"]);  
+    const tweetFields = "id,author_id,text,created_at" 
+    const url2 = `https://api.twitter.com/2/users/${userId}/tweets`;
+    const postsResponse = await fetch(url2, options)     
+    const postsData = await postsResponse.json()
+    //checks if the user has posts and returns the corresponding response object in case not
+    if(!postsData.hasOwnProperty("data")){
+        return responseForServer = {"posts":"0", "error":true, "error-message":"The user has no posts", "status":"204"}; 
+    }
+    //-------------------------------------------------
+    data = postsData.data;
+    //it got here which means user exists and also has posts
+    //will pass the posts' text as an array within the response object    
+    for(let i = 0; i < data.length; i++) {
+        data[i] = data[i].text;
+    }
+    return responseForServer = {"posts":data, "error": false, "error-message":"We gucci", "status":"200"};
 }
  
